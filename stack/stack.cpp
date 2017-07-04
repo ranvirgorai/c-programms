6236b0ccf8c3f8e2614d4f24a81628b314d80dbd
Ranvir Gorai <gorairanvir@gmail.com>
2017-07-04 15:27:30 +0530

    file starctur changed

---
+++ stack/stack.cpp
@@ -0,0 +1,20 @@
+/*
+* @Author: Ranvir
+* @Date:   2017-07-03 23:57:33
+* @Last Modified by:   Ranvir Gorai
+* @Last Modified time: 2017-07-04 10:13:10
+*/
+
+#include <iostream>
+
+struct stacktype
+{
+	
+};
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
